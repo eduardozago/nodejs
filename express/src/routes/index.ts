a3d176c83261054708a3d5f8b89a6b76f85125d7
eduardozago <zagoe22@gmail.com>
2023-01-23 16:46:34 -0300

    exploring mustache tools

--- express/src/routes/index.ts
+++ express/src/routes/index.ts
@@ -4,8 +4,13 @@ const router = Router();
 
 router.get('/', (req: Request, res:Response) => { 
     let user: string = 'John';   
-    res.render('home', {
-        user: user
+    res.render('pages/home', {
+        user: user,
+        products: [
+            {title: 'Product 1', price: 16},
+            {title: 'Product 2', price: 8},
+            {title: 'Product 3', price: 20}
+        ]
     });
 });
 
